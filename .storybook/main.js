d9458469dc5ede5f8e023e3d64b96b2593e4d97f
algomachine007 <benneth@bejamas.io>
2022-09-17 20:34:48 +0100

    removal of task component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,33 +1,33 @@
-// module.exports = {
-//   "stories": [
-//     "../stories/**/*.stories.mdx",
-//     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-//   ],
-//   "addons": [
-//     "@storybook/addon-links",
-//     "@storybook/addon-essentials",
-//     "@storybook/addon-interactions"
-//   ],
-//   "framework": "@storybook/react",
-//   "core": {
-//     "builder": "@storybook/builder-webpack5"
-//   }
-// }
-
 module.exports = {
-  stories: ['../src/components/**/*.stories.js'],
-  staticDirs: ['../public'],
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
     '@storybook/addon-interactions',
   ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
   },
-  features: {
-    interactionsDebugger: true,
-  },
 };
+
+// module.exports = {
+//   stories: ['../src/components/**/*.stories.js'],
+//   staticDirs: ['../public'],
+//   addons: [
+//     '@storybook/addon-links',
+//     '@storybook/addon-essentials',
+//     '@storybook/preset-create-react-app',
+//     '@storybook/addon-interactions',
+//   ],
+//   framework: '@storybook/react',
+//   core: {
+//     builder: '@storybook/builder-webpack5',
+//   },
+//   features: {
+//     interactionsDebugger: true,
+//   },
+// };
